a5df8a9f50a08853d6b69fa0bdada53dffd38cbf
Rick Clinton <307349629+Rick-Clinton-jpg@users.noreply.github.com>
2026-09-11 02:56:42 +0530

    Add src/mirage/__init__.py

---
+++ src/mirage/__init__.py
@@ -0,0 +1,8 @@
+"""Mirage containment-evidence verifier."""
+
+from .model import ControlResult, ReportStatus, VerificationReport
+from .verifier import verify_file
+
+__all__ = ["ControlResult", "ReportStatus", "VerificationReport", "verify_file"]
+__version__ = "0.1.0"
+
